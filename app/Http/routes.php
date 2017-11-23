0b26afaa922785aa233842819dbd67a9ed176c2c
Alireza <lovelylektus@yahoo.com>
2017-11-23 06:11:55 -0800

    Some miDdleWare Added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,4 +21,7 @@ Route::get('/home', 'HomeController@index');
 	Route::get('/admin', function(){
 		return view('admin.index');
 	});
-Route::resource('/admin/users' , 'AdminUsersController');
+
+Route::group(['middleware'=>'admin'] , function(){
+	Route::resource('/admin/users' , 'AdminUsersController');
+});
